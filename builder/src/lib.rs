f7684e7d2e121a4f56e939dfbf981ef86b601387
Marc Hoffman <mhoffman@qti.qualcomm.com>
2023-11-15 00:44:41 -0800

    builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,20 @@
 use proc_macro::TokenStream;
-
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = Ident::new(&bname, name.span());
+    //eprintln!("{:#?}", ast);
 
-    unimplemented!()
+    let expanded = quote! {
+    pub struct #bident {
+    }
+    impl #name {
+        pub fn builder() -> #bident { #bident {} }
+    }
+    };
+    expanded.into()
 }
